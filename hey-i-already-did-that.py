39b082cc4604ae26fad9d61bc4648d5d3d0eed8d
Jabr0ski <51535527+Jabr0ski@users.noreply.github.com>
2021-12-04 00:52:10 +0800

    Update hey-i-already-did-that.py

--- hey-i-already-did-that.py
+++ hey-i-already-did-that.py
@@ -1,7 +1,35 @@
 def solution(n, b):
-    digits = []
+    z = n
+    cycle = 0
+    nDigits = []
+    for i in str(z):
+        nDigits.append(int(i))
+    k = len(nDigits)
 
-    k = len(n)
-    print(k)
+    while str(n) != z or cycle < 1:
+        digits = []
+        for i in str(z):
+            digits.append(int(i))
 
-solution(123, 10)
+        digitsAsc = sorted(digits)
+        digitsDes = sorted(digits, reverse = True)
+
+        x = ""
+        for i in digitsDes:
+            x += str(i)
+        x = int(x)
+
+        y = ""
+        for i in digitsAsc:
+            y += str(i)
+        y = int(y)
+
+        z = x - y
+        z = str(z)
+        while len(z) < k:
+            z = "0" + z
+        print(z, str(n))
+        cycle += 1
+    print(cycle)
+
+solution(1211, 10)
